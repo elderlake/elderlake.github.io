3ec84fadf4bdab0465ff77e25121c92b9ffb8dee
Sabastian Auth <sabastian@elderlake.com>
2021-02-18 16:10:53 -0500

    New website

---
+++ js/about.be437bb2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),o={class:"about"},c=Object(a["f"])("h1",null,"This is an about page",-1);function s(n,t){return Object(a["o"])(),Object(a["e"])("div",o,[c])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.be437bb2.js.map
